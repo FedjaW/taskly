6e2d0b20c165b9499ed72827742a835256155d6f
FedjaW <f.wilms5189@gmail.com>
2025-03-03 21:52:49 +0100

    completed items

---
+++ components/ShoppingListItem.tsx
@@ -0,0 +1,91 @@
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { theme } from "../theme";
+
+type Props = {
+  name: string;
+  isCompleted?: boolean;
+};
+
+export function ShoppingListItem({ name, isCompleted }: Props) {
+  const handleDelete = () => {
+    Alert.alert(
+      `Are you sure you want to delete ${name}?`,
+      "It will be gone for good",
+      [
+        {
+          text: "Yes",
+          onPress: () => console.log("Ok"),
+          style: "destructive",
+        },
+        {
+          text: "Cancel",
+          style: "cancel",
+        },
+      ],
+    );
+  };
+
+  return (
+    <View
+      style={[
+        styles.itemContainer,
+        isCompleted ? styles.completedContainer : undefined,
+      ]}
+    >
+      <Text
+        style={[
+          styles.itemText,
+          isCompleted ? styles.completedText : undefined,
+        ]}
+      >
+        {name}
+      </Text>
+      <TouchableOpacity
+        style={[
+          styles.button,
+          isCompleted ? styles.completedButton : undefined,
+        ]}
+        onPress={handleDelete}
+        activeOpacity={0.8}
+      >
+        <Text style={styles.buttonText}>Delete</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  completedContainer: {
+    backgroundColor: theme.colorLightGrey,
+    borderBottomColor: theme.colorLightGrey,
+  },
+  completedButton: {
+    backgroundColor: theme.colorGrey,
+  },
+  completedText: {
+    textDecorationLine: "line-through",
+    textDecorationColor: theme.colorGrey,
+    color: theme.colorGrey,
+  },
+  itemText: { fontSize: 18, fontWeight: "200" },
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 8,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: theme.colorWhite,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
+  },
+});
